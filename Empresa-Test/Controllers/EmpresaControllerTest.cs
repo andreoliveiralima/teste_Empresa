b9134d00df17537fec213f135a9ad2809ff06c8f
andreoliveiralima <andre.oliveira.lima@hotmail.com>
2022-05-30 10:50:35 -0300

    ajuste

--- Empresa-Test/Controllers/EmpresaControllerTest.cs
+++ Empresa-Test/Controllers/EmpresaControllerTest.cs
@@ -7,18 +7,20 @@ using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Teste_Empresa.Domain;
+using Teste_Empresa.Interface;
 
 namespace Empresa_Test.Controllers
 {
     public class EmpresaControllerTest
     {
         private readonly Mock<ILogger<EmpresaController>> _mockLogger;
-        private readonly Mock<List<Empresa>> _mockEmpresas;
+        
+        private readonly Mock<ICarregaEmpresas> _mockCarregaEmpresa;
 
         public EmpresaControllerTest()
         {
             _mockLogger = new Mock<ILogger<EmpresaController>>();
-            _mockEmpresas = new Mock<List<Empresa>>();
+            _mockCarregaEmpresa = new Mock<ICarregaEmpresas>();
         }
 
         [Theory(DisplayName ="Sucesso ao buscar empresa")]
@@ -26,20 +28,13 @@ namespace Empresa_Test.Controllers
         [InlineData(2)]
         public async Task BuscarEmpresa(int id)
         {
-            var command = new EmpresaController(_mockLogger.Object);
+            var command = new EmpresaController(_mockLogger.Object, _mockCarregaEmpresa.Object);
             var response = await command.Get(id);
 
             Assert.Equal(HttpStatusCode.OK.GetHashCode(), ((ObjectResult)response).StatusCode);
         }
 
-        [Fact(DisplayName = "Sucesso ao carregar empresas")]
-        public void CarregarEmpresa()
-        {
-            var command = new EmpresaController(_mockLogger.Object);
-            var result = command.CarregaEmpresas(_mockEmpresas.Object);
-
-            Assert.True(result);
-        }
+        
 
     }
 }
---
+++ Empresa-Test/Repository/CarregaEmpresasRepositoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Teste_Empresa.Domain;
+using Teste_Empresa.Repository;
+using Xunit;
+
+namespace Empresa_Test.Repository
+{
+    public class CarregaEmpresasRepositoryTest
+    {
+        private readonly Mock<ILogger<CarregaEmpresasRepository>> _mockLogger;
+        private readonly Mock<List<Empresa>> _mockEmpresas;
+        private readonly CarregaEmpresasRepository _carregaEmpresasRepository;
+
+        public CarregaEmpresasRepositoryTest()
+        {
+            _mockLogger = new Mock<ILogger<CarregaEmpresasRepository>>();
+            _mockEmpresas = new Mock<List<Empresa>>();
+            _carregaEmpresasRepository = new CarregaEmpresasRepository(_mockLogger.Object); 
+        }
+
+        [Fact(DisplayName = "Sucesso ao carregar empresas")]
+        public void CarregarEmpresasTest()
+        {
+            
+            var result = _carregaEmpresasRepository.CarregarEmpresas(_mockEmpresas.Object);
+
+            Assert.True(result);
+        }
+    }
+}
